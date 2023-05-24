ec3ee69290be5066a6576e962606d9b3202d8d56
alexey.zhila <aleksey.zhila@kartoteka.ru>
2023-05-24 18:15:53 +0300

    добавляет loader и action для загрузки списка контактов и просмотра контакта

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./error-page";
 
 import "./index.css";
-import Root from "./routes/root";
+
+import ErrorPage from "./error-page";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+    ],
   },
 ]);
 
